2d95c273c05a4ad6272516bfd4d096d38cdc8e4c
Andrew Favaloro <andrew.favaloro@gmail.com>
2021-05-08 12:44:31 -0400

    VERY basic login component

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -1,3 +1,35 @@
+import { useContext, useState } from "react";
+import { backendConnection } from "../Services/backendConnection";
+
 export default function LogIn() {
-  return <>LogIn</>;
+  const backend = useContext(backendConnection);
+  const loginParams = { username: "", password: "" };
+  const [isLoading, setLoading] = useState(false);
+
+  const blurHandler = (fieldName) => (blurEvent) => {
+    loginParams[fieldName] = blurEvent.target.value;
+  };
+  const loginClickHandler = async () => {
+    setLoading(true);
+    await backend.AuthConnection.logIn(loginParams);
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        placeholder="email@em.ail"
+        onBlur={blurHandler("username")}
+      />
+      <input
+        type="password"
+        placeholder="hunter2"
+        onBlur={blurHandler("passwowrd")}
+      />
+      <button disabled={isLoading} onClick={loginClickHandler}>
+        Log In
+      </button>
+    </div>
+  );
 }
