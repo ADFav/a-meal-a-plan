9f07f7f5495799d4e01d8cfca84f0a9301fcda62
Andrew Favaloro <andrew.favaloro@gmail.com>
2021-05-08 11:45:08 -0400

    Stubed Routes

--- src/Routers/loggedIn.jsx
+++ src/Routers/loggedIn.jsx
@@ -1,16 +1,5 @@
 import { Switch, Route } from "react-router-dom";
-import Recipe from "../Routes/Recipe/Recipe";
-import Recipes from "../Routes/Recipes/Recipes";
 
 export default function LoggedIn() {
-  return (
-    <Switch>
-      <Route path="recipes/">
-        <Recipes />
-      </Route>
-      <Route path="/recipe/">
-        <Recipe />
-      </Route>
-    </Switch>
-  );
+  return <Switch></Switch>;
 }
